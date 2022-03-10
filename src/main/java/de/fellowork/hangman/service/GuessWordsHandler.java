a4ec9c179030e8a75c476b76a6a170b78280db80
Mayumi <mayumi.kaliciak@gmail.com>
2022-03-10 15:20:16 +0100

    refactored html

--- src/main/java/de/fellowork/hangman/service/GuessWordsHandler.java
+++ src/main/java/de/fellowork/hangman/service/GuessWordsHandler.java
@@ -2,40 +2,36 @@ package de.fellowork.hangman.service;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Component
 public class GuessWordsHandler {
-    List<String> randomWord;
 
+    private final Random random = new Random();
+    private List<List<String>> guessWords = new ArrayList<>();
 
-    public List<HangmanLetter> getRandomGuessWord() {
-
-//        return List.of("T", "E", "L", "E", "K", "O", "M").stream().map(HangmanLetter::new).toList();
-
-        randomizeWords();
-
-        return randomWord.stream().map(HangmanLetter::new).toList();
-    }
-
-    private List<String> randomizeWords() {
-
-        List<List<String>> guessWords = new ArrayList<>();
-
+    @PostConstruct
+    public void setupHandler(){
         guessWords.add(List.of("h", "e", "r", "z", "a", "l", "l", "e", "r", "l", "i", "e", "b", "s", "t"));
         guessWords.add(List.of("t", "e", "l", "e", "k", "o", "m", "m", "i", "t", "a", "r", "b", "e", "i", "t", "e", "r"));
         guessWords.add(List.of("t", "e", "l", "e", "f", "o", "n"));
         guessWords.add(List.of("e", "n", "z", "y", "k","l", "o", "p", "ä", "d", "i", "e" ));
         guessWords.add(List.of("a", "l", "l", "g", "e", "m", "e", "i", "n", "m", "e", "d", "i", "z", "i", "n", "e", "r"));
         guessWords.add(List.of("t", "e", "l", "e", "f", "o", "n", "k", "a", "b", "e", "l"));
+    }
 
-        int nextWord = ThreadLocalRandom.current().nextInt(0, guessWords.size());
-        randomWord = guessWords.get(nextWord);
+    public List<HangmanLetter> getRandomGuessWord() {
 
-        return randomWord;
+        int randomEntry = random.nextInt(guessWords.size());
 
+        return guessWords.get(randomEntry)
+                .stream()
+                .map(HangmanLetter::new)
+                .toList();
     }
 
 }
